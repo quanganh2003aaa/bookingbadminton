be90d92ae1930031894cd7e17085a6c8a58d2176
quankane <quanducbui2017@gmail.com>
2026-01-06 22:01:25 +0700

    QuanBD

--- src/main/java/com/example/bookingbadminton/BookingbadmintonApplication.java
+++ src/main/java/com/example/bookingbadminton/BookingbadmintonApplication.java
@@ -1,9 +1,14 @@
 package com.example.bookingbadminton;
 
+import com.example.bookingbadminton.config.keycloak.KeycloakProperties;
+import com.example.bookingbadminton.config.properties.AdminInfoProperties;
+import com.example.bookingbadminton.config.properties.FileUploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({AdminInfoProperties.class, KeycloakProperties.class, FileUploadProperties.class })
 public class BookingbadmintonApplication {
 
 	public static void main(String[] args) {
